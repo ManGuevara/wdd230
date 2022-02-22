86c5b30b4fc934332d478c1cd9ce24f6a19e9554
ManGuevara <gue21024@byui.edu>
2022-02-22 18:03:33 -0500

    updating the windchill formula

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -8,11 +8,14 @@ wSpeed.innerHTML = "5";
 let windChilldFormula= function(t,s){
     let windChill="";
     if(t<=10 && s>=4.8){
-        windChill= 35.74+0.6215*t-35.75*(s**0.16)+0.4275*t*(s**0.16)
+        windChill= parseFloat(13.12+0.6215*t-11.37*(s**0.16)+0.3965*t*(s**0.16))
 
-        return windChill
+        return windChill.toFixed(2)
 
     }else {return "N/A"}
 };
 
 document.getElementById("factor").innerHTML= windChilldFormula(parseFloat(temp.innerHTML) , parseFloat(wSpeed.innerHTML))
+
+
+
