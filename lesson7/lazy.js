a5eb7a8bfe27944b59f844dfe25aae0e8e94b378
ManGuevara <gue21024@byui.edu>
2022-02-22 20:18:46 -0500

    lazy images updated

--- lesson7/lazy.js
+++ lesson7/lazy.js
@@ -1,16 +1,34 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    treshold: 0,
+    rottMargin: "0px 0px 50px 0px"
+};
+
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload =()=>{
         image.removeAttribute('data-src');
     };
 };
-
-imagesToLoad.forEach((img)=>{
+if('IntersectionObserver' in window){
+    const imgObserver = new IntersectionObserver((entries, imgObserver)=>{
+        entries.forEach((entrie)=>{
+            if(entrie.isIntersecting){
+                loadImages(entrie.target);
+                imgObserver.unobserve(entrie.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img)=>{
+        imgObserver.observe(img);
+    });
+}else{
+    imagesToLoad.forEach((img)=>{
     loadImages(img);
-});
+    });
 
-
+};
 
 
 
