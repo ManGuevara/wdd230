f2630847b641ba1ea28f8dfe0a1dc080403a8b55
ManGuevara <gue21024@byui.edu>
2022-03-12 07:41:29 -0500

    updating

--- chamber/js/wheaterapi.js
+++ chamber/js/wheaterapi.js
@@ -7,12 +7,12 @@ const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=3936456&units
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    //console.log(jsObject);
     document.querySelector('#current-temp').textContent = jsObject.main.temp.toFixed(0);
 
   const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
   const desc = jsObject.weather[0].description;
-  //document.querySelector('#icon-src').textContent = iconsrc;
+  //accessing elements from the DOM;
   document.querySelector('#weathericon').setAttribute('src', iconsrc);
   document.querySelector('#weathericon').setAttribute('alt', desc);
   document.querySelector('figcaption').textContent = desc;
