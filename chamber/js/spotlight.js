834be25a1df4dcf6bfbe5465520e62304fafe84b
ManGuevara <gue21024@byui.edu>
2022-03-19 12:11:16 -0500

    updates

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -13,7 +13,26 @@ fetch(requestURL)
         companyArray.push(golden)
         companyArray.push(silver)
         companyArray.push(bronze)
-        console.log(logosinfo)
+        /*console.log(companyArray)*/
+        let spotlight=document.querySelector(".spotlight")
+        
+        companyArray.forEach((company,index)=>{
+            let spot = document.createElement("div")
+            let h1 = document.createElement("h1")
+            let imageSpot= document.createElement("img")
+            let p1 = document.createElement("p")
+            let p2 = document.createElement("p")
+            
+            h1.innerHTML = company.name
+            imageSpot.src = company.imageurl
+            p1.innerHTML = company.phone
+            p2.innerHTML = company.address
+            spot.classList.add(`spot${index+1}`)
+            spot.append(h1)
+            spot.append(imageSpot)
+            spot.append(p1)
+            spot.append(p2)
+            spotlight.append(spot)
+        })
         });
 
-console.log("hola")
